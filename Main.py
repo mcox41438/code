0fb6c64f069bf120f4e55ba33e14261ef383150c
mcox41438 <mason.cox@controlsouthern.com>
2024-06-12 20:19:13 +0000

    yep

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
-
+# how
 
 class MyCustomWindow(tk.Frame):
     def __init__(self, parent):
